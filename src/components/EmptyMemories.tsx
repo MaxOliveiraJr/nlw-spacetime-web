0390e3d8423f568cb998389908f47d0ee8cbc665
Max Oliveira <max-oliveira@outlook.com.br>
2023-05-19 22:56:17 -0300

    feat: criando components

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembraça, comece a{' '}
+        <a href="" className="hover:text-grey-50 underline">
+          criar agora
+        </a>
+      </p>
+    </div>
+  )
+}
