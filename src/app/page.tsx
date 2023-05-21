42a84fbb536c3d128ea48eecf1c4a50b9483617b
Max Oliveira <max-oliveira@outlook.com.br>
2023-05-20 21:54:37 -0300

    feat: profile concluido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
+import { Profile } from '@/components/Profile'
 export default function Home() {
   const isAuthenticated = cookies().has('token')
   console.log(isAuthenticated)
@@ -13,7 +14,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full  bg-purple-700 opacity-50 blur-full"></div>
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
         {/* Sign in */}
-        {isAuthenticated ? <>oi</> : <SignIn />}
+        {isAuthenticated ? <Profile /> : <SignIn />}
 
         {/* Hero */}
         <Hero />
